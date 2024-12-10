3b8c5207502854e164ccf2265ef309bc5eae842d
diego <diegoffa01@gmail.com>
2024-12-10 13:03:51 -0300

    att

--- assets/scripts/2teladelongim.js
+++ assets/scripts/2teladelongim.js
@@ -1,23 +1,20 @@
-function validarFormulario() {
-    // Obtendo os valores dos campos de login
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
 
-    // Validação do campo de email
-    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    if (!emailPattern.test(email)) {
-        alert("Por favor, insira um email válido.");
-        return false; // Impede o redirecionamento
+    function validarFormulario() {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (!emailPattern.test(email)) {
+            alert("Por favor, insira um email válido.");
+            return false; 
+        }
+
+        if (password.length < 6) {
+            alert("A senha deve ter pelo menos 6 caracteres.");
+            return false;
+        }
+
+        // Redirecionamento após validação bem-sucedida
+        window.location.href = "http://127.0.0.1:5501/2%20carrinho.html"; // Use o caminho relativo correto aqui.
+        return true;
     }
-
-    // Validação do campo de senha
-    if (password.length < 6) {
-        alert("A senha deve ter pelo menos 6 caracteres.");
-        return false; // Impede o redirecionamento
-    }
-
-    // Se todas as validações passarem, redireciona para a próxima página
-    window.location.href = "http://127.0.0.1:5500/1-tela%20inicial.html"; // Substitua pelo caminho correto
-
-    return true; // O redirecionamento ocorrerá
-}
