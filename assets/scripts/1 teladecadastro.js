a807ee5947103d3304a2b4d73e5ca8ee63556791
diego <diegoffa01@gmail.com>
2024-12-12 10:19:18 -0300

    endd

--- assets/scripts/1 teladecadastro.js
+++ assets/scripts/1 teladecadastro.js
@@ -1,8 +1,9 @@
 function validarFormulario() {
-    // Obtendo os valores dos campos de login
+    //  Aqui vai btendo os valores dos campos de login
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    // Validação do campo de email
+
+    // Aqui vai fazer a validação do campo de email
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     if (!emailPattern.test(email)) {
         alert("Por favor, insira um email válido.");
@@ -13,7 +14,7 @@ function validarFormulario() {
         alert("A senha deve ter pelo menos 6 caracteres.");
         return false;
     }
-    // Se todas as validações passarem, redireciona para a próxima página
+    // Se todas as validações passarem,sera redirecionado para a próxima página
     window.location.href = "http://127.0.0.1:5501/1-tela%20inicial.html"; 
     return true; 
     }
